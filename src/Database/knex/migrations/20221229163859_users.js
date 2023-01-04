b9e1b78129c883c1bc3f58148acbaf94cecccee5
FabioVascão <fabiosousa2691@hotmail.com>
2023-01-04 10:54:40 -0300

    notes

--- src/Database/knex/migrations/20221229163859_users.js
+++ src/Database/knex/migrations/20221229163859_users.js
@@ -1,8 +1,9 @@
-exports.up = knex => knex.schema.createTable("users", table => {
+exports.up = knex => knex.schema.createTable("movies_notes", table => {
     table.increments("id");
-    table.text("Name");
-    table.text("Email");
-    table.text("Password");
+    table.text("Title");
+    table.text("Description");
+    table.string("Rating", 5);
+    table.integer("user_id").references("id").inTable("users");
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
 });
