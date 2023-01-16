769e12297a5b3344e0ae58eb0fc88c45a8ef14b3
FabioVascão <fabiosousa2691@hotmail.com>
2023-01-16 14:03:13 -0300

    index Notes

--- src/Routes/notes.Router.js
+++ src/Routes/notes.Router.js
@@ -9,6 +9,7 @@ const notesController = new NotesController();
 notesRouter.post("/:user_id", notesController.create);
 notesRouter.get("/:id", notesController.show);
 notesRouter.delete("/:id", notesController.delete);
+notesRouter.get("/", notesController.index);
 
 module.exports = notesRouter;
 
